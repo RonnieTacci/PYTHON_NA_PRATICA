86d91ebfbb6643e327a8a32ae66a972a1ebb63d8
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-10 22:51:03 -0300

    Exemplos-tipos de dados

--- Aula8.py
+++ Aula8.py
@@ -10,5 +10,16 @@
 
 meu_texto = "Minha palavra"
 print(type(meu_texto))      # ( type -> mostrou o tipo da variavel)
+print(meu_texto)
+
+variavel_int = 100
+print(variavel_int)
 
 
+print(type(100.50))
+
+esta_chovendo = True
+luzAcesa = False
+
+print(esta_chovendo)
+print(luzAcesa)
