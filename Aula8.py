0479ffd1e0c9a5711a76a4b2de6273cc88f357f0
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-10 22:03:01 -0300

    Tipos de dados

---
+++ Aula8.py
@@ -0,0 +1,14 @@
+# Tipos de dados
+
+
+''' str : dados textuais (strings)
+    int : numeros inteiros (sem casa decimal)
+    float : numeos decimais ( com pate decimal, ponto)
+    bool : valores logicos (True e False)'''
+
+#exemplos
+
+meu_texto = "Minha palavra"
+print(type(meu_texto))      # ( type -> mostrou o tipo da variavel)
+
+
