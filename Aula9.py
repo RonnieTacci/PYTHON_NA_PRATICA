c62e3be869cc8f42fc58dd1027202c425e29203e
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-13 11:46:47 -0300

    Exercios de Operadores no Python

--- Aula9.py
+++ Aula9.py
@@ -34,3 +34,30 @@ b = False
 print( a and b) # and (E) ambos tem que ser verdadeiro
 print(a or b) # or (ou) um ou outro valor tem que ser verdadeiro
 print(not a) # invertendo o valor logico
+
+
+# Exercicios
+
+'''Crie duas variaveis, com valores inteiros.
+Faça com essas variaveis todas as operações aritmeticas mostradas na aula.
+Armazene os resuldados de cada operação e utilize fstrings para imprimir cada resultado no seguinte modelo:
+'Resultado da soma:10'  '''
+
+nuInt1 = 20
+nuInt2 = 20
+
+somaInt = nuInt1 + nuInt2
+subtacaoInt = nuInt1 - nuInt2
+vezesInt = nuInt1 * nuInt2
+divisaoInt = nuInt1 / nuInt2
+
+print(f"Resultado da soma: {somaInt}")
+print(f"Resultado da subtração: {subtacaoInt}")
+print(f"Resultado da multiplicação: {vezesInt}")
+print(f"Resultado da divisão: {divisaoInt}")
+
+potenciaInt = nuInt1 ** nuInt2
+restoInt = nuInt1 % nuInt2
+
+print(f"Resultado da potencia: {potenciaInt}")
+print(f"Resultado do resto da divisão: {restoInt}")
