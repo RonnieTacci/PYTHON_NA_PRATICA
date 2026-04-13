d790f236cc9dffa4defdf02c9496f3466e5c8996
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-13 11:28:54 -0300

    operadores python

---
+++ Aula9.py
@@ -0,0 +1,36 @@
+# Operadores de Python
+
+''' Aritimeticos - usados paa faze calculos matematicos
+    Relacionais - usados para comparar valores
+    Logicos - usados para combinar condições, normalmente booleanos'''
+
+# Aritimetico
+
+x = 30
+y = 3
+
+print( x + y)
+print( x ** y) #POTENCIA (X elevado à Y)
+print( x // y) #divisão mostrando somente o resultado INTEIRO
+print( x % y)  #resto da divisão
+
+a = 0.1
+b = 0.2
+resultado = a + b
+
+print ( a + b)
+print (round(resultado,1))
+
+# Relacionais
+
+print (a == 0.1) #comparação
+print (a != b) #diferente
+
+# Logicos
+
+a = True
+b = False
+
+print( a and b) # and (E) ambos tem que ser verdadeiro
+print(a or b) # or (ou) um ou outro valor tem que ser verdadeiro
+print(not a) # invertendo o valor logico
