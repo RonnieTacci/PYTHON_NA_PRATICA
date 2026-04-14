3ff831e54b3ee03a6c9cc330429c2e0fae35e938
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-14 16:09:16 -0300

    Entrada de dados

---
+++ Aula10.py
@@ -0,0 +1,11 @@
+# Input de dados (entrada)
+
+nome = input("Informe seu nome: ")
+print(f"Bme-vindo {nome}")
+
+idade = input("Informe sua idade: ")        
+print(type(idade))
+print(f"Sua idade é {idade}, permissão para entrar.")
+
+# idade = int(input("Informe sua idade: ")) ou variavel = float(input("Informe sua idade: "))
+
