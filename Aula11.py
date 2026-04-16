256c512d348b8beba5f124c391bb637e14f190c5
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-16 17:55:32 -0300

    Exercicios Estruturas condicionais (if, elif, else)

--- Aula11.py
+++ Aula11.py
@@ -49,3 +49,32 @@ elif temperatura >= 20:
     print("Esta agradavel!")
 else:
     print("Esta muito frio!")
+
+'''Peça ao usuario para digitar a idade de uma pessoa.Com base nessa idade,
+informe o valor da tarifa de tranporte que tera que ser paga.
+Se a idade for menos de 6 anos, tarifa sera gratuita. Se for acima ou igual a 6 anos
+e abaixo de 18 anos, meia tarifa(5 reais). Se for a partir de 18 anos e abaixo de 60 anos,
+tarifa inteira(10 reais). Se for idoso,acisa de 60 anos, tarifa gratuita.'''
+
+idade_tarifa = int(input("Informe sua idade: "))
+
+if (idade_tarifa < 6) or (idade_tarifa > 60):
+    print("Tarifa gratuita.")
+elif (idade_tarifa >= 6) and (idade_tarifa <18):
+    print("Meia tarifa (5 reais) ")
+else:
+    print("Tarifa inteira (10 reais)")
+
+    
+'''Peça ao usuario para digitar um username e uma senha.Considera que o 
+usuario correto é "admin" e que a senha correta é python2026.
+Se as credenciais estiverem corretas, exiba "Login bem sucedido!.
+Do contrario, exiba que as credenciais estao incorretas."'''
+
+userName = input("Informe nome de usuario: ")
+senhaClientes = input("Informe senha para login. ")
+
+if (userName == "admin") and (senhaClientes == "python2026"):
+    print("Login bem sucedido!")
+else:
+    print("Credenciais incorretas")
