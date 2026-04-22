60ab8c629e7ac2dceb5785054a36534ccea15c81
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-22 17:03:41 -0300

    Exemplos de Funções

--- Aula14.py
+++ Aula14.py
@@ -55,3 +55,43 @@ def exibir_frase(nome, altura, linguagem):
     print(f"{nome} tem {altura}m e programa em {linguagem}.")
 exibir_frase("Ronnie", 1.71, "Python")
 
+def calcular_media(numero_aluno):
+    nota1 = float(input("Digite a primeira nota: "))
+    nota2 = float(input("Digite a segunda nota: "))
+    nota3 = float(input("Digite a terceira nota: "))
+
+    media = (nota1 + nota2 + nota3) / 3
+    media = round(media, 1)
+
+    print(f"Média do aluno {numero_aluno}: {media}")
+
+calcular_media(1)
+calcular_media(2)
+
+def calcular_veloc_media(distancia, tempo):
+    resultado = distancia / tempo
+    resultado = round(resultado,2)
+
+    return resultado
+
+distancia = float(input("Informe a distancia em (km): "))
+tempo = float(input("Informe o tempo em (h): "))
+
+vel_media = calcular_veloc_media(distancia, tempo)
+
+print(f"Velocidade média: {vel_media} km/h")
+
+def notas_aprovados(lista_notas):
+    lista_aprovados = []
+
+    for nota in lista_notas:
+        if nota >= 7.0:
+            lista_aprovados.append(nota)
+    
+    return lista_aprovados
+
+lista = [10.0, 4.4, 7.1, 5.9, 9.1]
+lista_resultante = notas_aprovados(lista)
+
+print("Notas aprovadas")
+print(lista_resultante)
