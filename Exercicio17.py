65e1c16ccfe51289c5e28148948e57b3277bb323
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-24 20:35:08 -0300

    exercicio 2

---
+++ Exercicio17.py
@@ -0,0 +1,9 @@
+#Aula 17 - Exercício 2
+
+'''Escreva uma função que receba um numero inteiro não negativo (n) e 
+uma string (s) como parametros e retorne uma string que (s) se repita exatamente (n) vezes''' 
+
+def repeat_str(repeat, string):
+    return repeat * string
+
+print(repeat_str(6, "Oi"))
