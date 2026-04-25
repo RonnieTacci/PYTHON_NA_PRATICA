02a772183ead10e8da8b1dad4d5433ad287c37e2
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-25 16:26:23 -0300

    Exercicio 3: numero sempre impar

--- Exercicio18.py
+++ Exercicio18.py
@@ -1,1 +1,15 @@
 #Aula 18 - Exercício 3
+
+'''Nesta tafera vocé recebe um numero e tranforma ele em negativo, mas talves o numero ja esteja negativo.'''
+
+def num_negativo( numero ):
+    if numero > 0:
+        return numero * -1
+    
+    return numero
+    
+
+print(num_negativo(8))
+print(num_negativo(1))
+print(num_negativo(0))
+print(num_negativo(-10))
