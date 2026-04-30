e5c82b714d23bca400dc296239f0dd8fd1b13154
Ronnie Tacci <rlonnierdl@gmail.com>
2026-04-29 23:17:55 -0300

    Aula 21 - Exercício 6

--- Exercicio21.py
+++ Exercicio21.py
@@ -1,1 +1,14 @@
 #Aula 21 - Exercício 6
+
+
+def invert(list):
+    inverted_list = []
+
+    for item in list:
+        inverted_item = item * -1
+        inverted_list.append(inverted_item)
+
+    return inverted_list
+
+input_list = [1, 2, 3, 4, 5]
+print(invert(input_list))
