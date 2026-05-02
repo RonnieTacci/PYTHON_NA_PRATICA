6d4626ad34f031ed57083c99dd019674d5bbd25e
Ronnie Tacci <rlonnierdl@gmail.com>
2026-05-01 23:09:30 -0300

    Encontrar menor valor na lista

--- Exercicio22.py
+++ Exercicio22.py
@@ -1,1 +1,16 @@
 #Aula 22 - Exercício 7
+
+def encontrar_menor(lista):
+    menor = lista[0]
+
+    for item in lista:
+        if item < menor:
+            menor = item
+    
+    return menor
+
+lista_entrada = [34, 15, 88, 2, 10]
+print(encontrar_menor(lista_entrada))
+
+lista_entrada = [34, 15, -88, 2, 10]
+print(encontrar_menor(lista_entrada))
