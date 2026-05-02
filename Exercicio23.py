ad5f9ecc154b1c368bf6f7b2129d55e3724bedbe
Ronnie Tacci <rlonnierdl@gmail.com>
2026-05-02 13:45:32 -0300

    Exercício 8- seculo

--- Exercicio23.py
+++ Exercicio23.py
@@ -1,1 +1,15 @@
 #Aula 23 - Exercício 8
+
+def seculo(ano):
+    parte_inteira = ano // 100
+    parte_decimal = ano % 100
+
+    if parte_decimal > 0:
+        parte_inteira += 1
+
+    return parte_inteira
+
+print(f"Seculo: {seculo(1999)}")
+print(f"Seculo: {seculo(2026)}")
+print(f"Seculo: {seculo(1978)}")
+print(f"Seculo: {seculo(1500)}")
