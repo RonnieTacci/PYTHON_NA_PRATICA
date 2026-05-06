bb11e89027f5de6307bdbd6f9c43908157831213
Ronnie Tacci <rlonnierdl@gmail.com>
2026-05-05 23:01:41 -0300

    menor, maior, diferenca

--- Exercicio25.py
+++ Exercicio25.py
@@ -1,1 +1,20 @@
 #Aula 25 - Exercício 10
+
+def diferenca_idades(listas_idades):
+    maior = listas_idades[0]
+    menor = listas_idades[0]
+
+    for item in listas_idades:
+        if item < menor:
+            menor = item
+        
+        if item > maior:
+            maior = item
+
+    diferenca = maior - menor
+    tupla_resultado = (menor, maior, diferenca)
+
+    return tupla_resultado
+
+lista_entrada = [75, 22, 5, 44, 12, 99]
+print(diferenca_idades(lista_entrada))
