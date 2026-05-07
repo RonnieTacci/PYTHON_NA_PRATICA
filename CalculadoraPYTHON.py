5b27fa3a9ba81b3c6b067f4191e7424c0786d408
Ronnie Tacci <rlonnierdl@gmail.com>
2026-05-06 22:30:23 -0300

    add funções operadores

---
+++ CalculadoraPYTHON.py
@@ -0,0 +1,16 @@
+def soma(a , b):
+    return a + b
+
+def subtracao(a , b):
+    return a - b
+
+def multiplicacao(a , b):
+    return a * b
+
+def divisao(a , b):
+    return a / b
+
+valor_1 = 10
+valor_2 = 20
+
+print(f"Valor 1 somado com valor :  {soma(valor_1, valor_2)}")
