c6e6aa17b9d4aca65862e35f4cd047109a59e5de
Ronnie Tacci <rlonnierdl@gmail.com>
2026-05-26 20:51:56 -0300

    Utilizando a biblioteca math no projeto da calculadora

--- CalculadoraMath.py
+++ CalculadoraMath.py
@@ -1,5 +1,7 @@
 #Utilizar biblioteca Math + CalculadoraPYHTON (PYTHON_NA_PRATICA- repositorio)
 
+from math import sqrt, log2     #importando somente raiz quadrada e logaritmo na base 2.
+
 def soma(a , b):
     return a + b
 
@@ -12,12 +14,20 @@ def multiplicacao(a , b):
 def divisao(a , b):
     return a / b
 
+def raiz_quadrada(a):
+    return sqrt(a)
+
+def logaritmo(a):
+    return log2(a)
+
 def exibir_menu():
     print("=== CALCULADORA ===")
     print("1 - SOMA")
     print("2 - SUBTRAÇÃO")
     print("3 - MULTIPLICAÇÃO")
     print("4 - DIVISÃO")
+    print("5 - RAIZ QUADRADA (DO RESULTADO ATUAL)")
+    print("6 - LOGARITMO NA BASE 2 (DO RESULTADO ATUAL)")
     print("0 - SAIR")
 
 def formatar_resultado(resultado):
@@ -28,7 +38,7 @@ def formatar_resultado(resultado):
     return resultado
 
 def main():
-    opcoes_validas = {"1", "2", "3", "4", "0"}
+    opcoes_validas = {"1", "2", "3", "4", "5", "6", "0"}
 
     try:
         resultado_atual = float(input("Digite o valor inicial: "))
@@ -49,15 +59,16 @@ def main():
 
         if opcao_escolhida not in opcoes_validas:       #not in -> não pertença
             print("\nOpção inválida.")
-            print("Opções válidas: 1, 2, 3, 4 e 0\n ")
+            print("Opções válidas: 1, 2, 3, 4, 5, 6 e 0\n ")
 
             continue        #não executa o restante, volta o while
 
-        try:
-            valor_operando = float(input("Digite o próximo valor do operando: "))
-        except ValueError:
-            print("Número inválido.")
-            continue
+        if opcao_escolhida in {"1", "2", "3", "4"}:
+            try:
+                valor_operando = float(input("Digite o próximo valor do operando: "))
+            except ValueError:
+                print("Número inválido.")
+                continue
 
 
         if opcao_escolhida == "1":
@@ -71,6 +82,16 @@ def main():
                 resultado_atual = divisao(resultado_atual,valor_operando)
             except ZeroDivisionError:
                 print("Não se pode dividir por zero.")
+        elif opcao_escolhida == "5":
+            try:
+                resultado_atual = raiz_quadrada(resultado_atual)
+            except ValueError:
+                print("Não é possível calcular raiz quadrada de número negativo.")
+        elif opcao_escolhida == "6":
+            try:
+                resultado_atual = logaritmo(resultado_atual)
+            except ValueError:
+                print("Logaritmo só é definido para número positivo e diferente de zero.")
 
 
 
